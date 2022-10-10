26ae1b63d6fec896cbe13c060d2f77ffbe6f39dd
Kaym24 <109117979+Kaym24@users.noreply.github.com>
2022-10-10 15:19:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var K = "The past never forgets!"; // defining a variable
+window.alert(K) //creates a window alert box on webpage
+var Food = "Foods I have eaten.", Favorite = "sushi<br>", Worst = "Chocolate", //defining a statement and naming it
+    Candy = "Swedish Fish", Fast = "Panda Express"; // adding multiple variables 
+ document.write(Favorite);//displaying a variable
+ document.write(10 + 11); //an expression being displayed
+ document.write("<br>Kay told Panda, \"You are such a loser!\"<br>\"Shut up!\" Panda responded.<br>"); //created a series of strings with backlashes
+ var Sent1 = "I\'m kind "; //creating a variable to add to another variable
+ var Sent2 = " of dumb.<br>"; //creating a variable to add to another variable
+ document.write(Sent1 + Sent2) //adding both variables together
+ document.write("\This is the beginning of the string" + " and this is the end of the string."); //concatenating the variables together
